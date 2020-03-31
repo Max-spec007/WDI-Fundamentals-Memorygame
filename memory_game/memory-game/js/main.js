21342932254faddd33c70ee540decc31b9523a13
Maxwell <maxtheflash007@gmail.com>
2020-03-31 19:12:49 -0400

    Second Commit

--- memory_game/memory-game/js/main.js
+++ memory_game/memory-game/js/main.js
@@ -1,16 +1,27 @@
 console.log("Up and running!");
+console.log("User flipped queen");
+console.log("User flipped king");
 
+let checkForMatch = function () {};
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ["queen","queen","king","king"];                                                                                                                                                             
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+alert( cards[0] );
 
+let cardsInPlay = [];
 
+cardsInPlay.push("queen");
+cardsInPlay.push("king");
 
+if (cardsInPlay.length === 2) {};
 
+alert(cardsInPlay);
 
+let cardOne= "queen";
+let cardTwo= "king";
+if (cardsInPlay[0] === cardsInPlay[1]);
 
+   alert("You found a match!");
+   
+
+   alert("Sorry, try again.");
